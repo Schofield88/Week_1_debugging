f6306be915a25b484cc1652bcadc3fb21efa1e48
Luke Phyall <lukeprospect@yahoo.com>
2019-02-05 11:54:18 +0000

    Adds puts to encode method. Method now works.

--- exercise_2.rb
+++ exercise_2.rb
@@ -1,10 +1,9 @@
 def encode(plaintext, key)
   cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
-  print cipher
   ciphertext_chars = plaintext.chars.map do |char|
     (65 + cipher.find_index(char).to_i).chr
   end
-  ciphertext_chars.join
+  puts ciphertext_chars.join
 end
 
 def decode(ciphertext, key)
